68f0ce5d94f9238177a326b686a4319b68bd8b85
Otto Ebeling <otto.ebeling@aalto.fi>
2013-03-23 20:03:15 +0200

    Debugt prints to storetodb.py

--- src/datastore/storetodb.py
+++ src/datastore/storetodb.py
@@ -6,11 +6,14 @@ from traceparser import *
 import json
 from threading import Lock
 import leveldb
-
+from time import time as systemtime
+saveName = "t206"
 def init():
 	global t, in_str
-	t = Trace("binaries/ex3/trace.txt")
-	memorySpace = FossileStream("binaries/ex3/dump")
+	#t = Trace("binaries/ex3/trace.txt")
+	t = Trace("../raw/trace.txt", False)
+	#memorySpace = FossileStream("binaries/ex3/dump")
+	memorySpace = FossileStream("../raw/t206.fossile")
 	in_str = bin_stream_file(memorySpace)
 
 def normalize(x):
@@ -21,9 +24,18 @@ def normalize(x):
 
 def dumpToDb(iterator, db):
 	#for details in changeMatrixIterator(t.iterate(), t, in_str):
+	count = 0
+	lastTime = -1
 	for details in iterator:
+		if count%100000 == 0:
+			print >>sys.stderr, count
+			if lastTime!= -1:
+				delay = systemtime() - lastTime
+				print "%d instr/sec" % (100000./delay)
+			lastTime = systemtime()
 		curTime, eip, instr, changeMatrix = details
 		newMatrix = {}
+		count += 1
 		if changeMatrix is None: continue
 
 		for key,value in changeMatrix.items():
@@ -40,9 +52,9 @@ def dumpToDb(iterator, db):
 
 print >>sys.stderr, "Using old engine"
 init()
-db = leveldb.LevelDB("./db/memcrypt_oldEngine")
+db = leveldb.LevelDB("./db/%s_oldEngine" % saveName)
 dumpToDb(changeMatrixIterator(t.iterate(), t, in_str, newEngine = False), db)
 print >>sys.stderr, "Using new engine"
 init()
-newDb = leveldb.LevelDB("./db/memcrypt_newEngine")
+newDb = leveldb.LevelDB("./db/%s_newEngine" % saveName)
 dumpToDb(changeMatrixIterator(t.iterate(), t, in_str, newEngine = True, suppressErrors = True), newDb)
