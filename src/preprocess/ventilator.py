62bf627adc4579ae4a085fe3f7fc84f0f8e7e839
Otto Ebeling <otto.ebeling@aalto.fi>
2013-08-08 14:37:17 +0300

    Indexes

--- src/preprocess/ventilator.py
+++ src/preprocess/ventilator.py
@@ -75,9 +75,15 @@ def delegator(traceFile, dumpFile, newEngine, suppressErrors, dbName):
 				message = readySignalRecv.recv_json()
 				log("Got quit signal from pid=%d" % message['pid'])
 				break
+
 	log("Writing metainfo")
 	db = client[dbName]
 	db.meta.insert( {'maxTime' : t-1 } )
+
+	log("Adding indexes")
+	db.instructions.ensure_index( {'time' : 1} )
+	db.reads.ensure_index( {'addr' : 1, 'time' : 1} )
+	db.writes.ensure_index( {'addr' : 1, 'time' : 1} )
 
 
 def process(traceFile, dumpFile):
