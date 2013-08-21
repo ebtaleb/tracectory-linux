5e04ef5a7e4e92d5fd5c1f10ddfb5080ab919bd5
Otto Ebeling <otto.ebeling@aalto.fi>
2013-08-21 13:42:28 +0300

    Added the exporter script to repo

---
+++ src/datastruct/mongoexport.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+from pymongo import Connection as MongoClient
+import os, sys
+
+if len(sys.argv)<3:
+	print "mongoexpory.py dbname collectionname"
+	printg "e.g. mongoexport.py t206 reads"
+	sys.exit(1)
+
+client = MongoClient()
+db = client[sys.argv[1]]
+
+coll = db[sys.argv[2]]
+print coll.count()
+for entry in coll.find():
+	print entry['addr'], entry['time']
